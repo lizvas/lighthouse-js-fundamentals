7ca6b9b8e92be4767edbc8fde44908c4ea4f4ac9
Elizabeth Vasquez <vasquezeliz@hotmail.com>
2018-12-29 04:32:29 +0900

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+while (repeat < 1) {
+  console.log(ingredients);
+  repeat++;
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var repeat = 0; repeat < 1; repeat++) {
+  console.log(ingredients);
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 7; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
